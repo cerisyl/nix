83da740f5618cc805bb3e6897148041274b50d69
Spencer Gunning <csit@cering.dev>
2025-08-01 20:06:20 -0500

    Add: ollama

---
+++ nixos/config/core/ollama.nix
@@ -0,0 +1,8 @@
+{ config, lib, pkgMap, myHostname, ... }:
+if myHostname == "lux" then {
+  services.ollama = {
+    enable        = true;
+    package       = pkgMap.ollama;
+    acceleration  = "cuda";
+  };
+} else {}
