10243df1abaa6135f1c7b3e5d331b9baca41da08
Spencer Gunning <csit@cering.dev>
2026-09-01 00:38:34 -0500

    Fix frp config

--- nixos/config/net/frp.sys.nix
+++ nixos/config/net/frp.sys.nix
@@ -2,31 +2,33 @@
 if myHostname == "astore" then {
   services.frp = {
     package = pkgs.frp;
-    instaces.default = {
-      enable      = true;
-      role        = "client";
-      serverAddr  = "51.83.161.40";
-      serverPort  = 7000;
-      auth.tokenSource = {
-        type      = "file";
-        file.path = config.sops.secrets.nanachi.path;
+    instances.default = {
+      enable    = true;
+      role      = "client";
+      settings  = {
+        serverAddr  = "51.83.161.40";
+        serverPort  = 7000;
+        auth.tokenSource = {
+          type      = "file";
+          file.path = config.sops.secrets.nanachi.path;
+        };
+        proxies = [
+          {
+            name        = "rdp";
+            type        = "tcp";
+            localIP     = "127.0.0.1";
+            localPort   = 3389;
+            remotePort  = 48885;
+          }
+          {
+            name        = "ssh";
+            type        = "tcp";
+            localIP     = "127.0.0.1";
+            localPort   = 50951;
+            remotePort  = 52599;
+          }
+        ];
       };
-      proxies = [
-        {
-          name        = "rdp";
-          type        = "tcp";
-          localIP     = "127.0.0.1";
-          localPort   = 3389;
-          remotePort  = 48885;
-        }
-        {
-          name        = "ssh";
-          type        = "tcp";
-          localIP     = "127.0.0.1";
-          localPort   = 50951;
-          remotePort  = 52599;
-        }
-      ];
     };
   };
 } else {}
